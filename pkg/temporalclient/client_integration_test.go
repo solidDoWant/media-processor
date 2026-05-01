2b4ef906d60cab054e9c0f1efe6b48cebbc43814
solidDoWant <fred.heinecke@yahoo.com>
2026-05-01 19:45:23 +0000

    set TEMPORAL_PROFILE = default for tests
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- pkg/temporalclient/client_integration_test.go
+++ pkg/temporalclient/client_integration_test.go
@@ -36,6 +36,7 @@ func isolateTemporalConfig(t *testing.T) {
 
 	require.NoError(t, os.WriteFile(emptyConfig, nil, 0o600))
 	t.Setenv("TEMPORAL_CONFIG_FILE", emptyConfig)
+	t.Setenv("TEMPORAL_PROFILE", "default")
 }
 
 func TestDialHappyPath(t *testing.T) {
